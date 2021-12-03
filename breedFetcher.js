fccce4f7de1bbe20c726f79f9e9349cd2af38ca2
James <jameshuang337@gmail.com>
2021-12-02 19:37:56 -0800

    added unit testing and modularity

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,16 @@
 const request = require('request');
-const input = process.argv.slice(2)
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + input[0], (error, response, body) => {
-    if (error && error.code === 'ENOTFOUND') {
-        console.log('URL not found');
-        process.exit(0);
-    };
-    // console.log('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // console.log('body:', body); // Print the HTML  
-    try {
-        const data = JSON.parse(body);
-        console.log(data[0].description);
-    } catch(err) {
-        console.log('Breed not found');
-    };
-    // console.log(typeof data);
-});
 
+const fetchBreedDescription = function(input, callback) {
+    let addressBeginning = 'https://api.thecatapi.com/v1/breeds/search?q='
+    request(addressBeginning + input, (error, response, body) => {
+            const data = JSON.parse(body);
+            //console.log(data[0].description)
+            callback(error, data[0].description)
+            // console.log(typeof data);
+    });
+};
+
+
+
+module.exports = { fetchBreedDescription };
